6bff8b117fe7f60ed6afa655a2d214304f6837f7
Evoless <amoogames519@gmail.com>
2025-07-20 19:21:40 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    """Query all books by a specific author"""
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()  # Using the related_name 'books'
+        return Book.objects.filter(author=author)
     except Author.DoesNotExist:
         return Book.objects.none()
 
 def get_books_in_library(library_name):
-    """List all books in a library"""
     try:
         library = Library.objects.get(name=library_name)
         return library.books.all()
@@ -17,16 +15,13 @@ def get_books_in_library(library_name):
         return Book.objects.none()
 
 def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian  # Using the related_name 'librarian'
-    except Library.DoesNotExist:
+        return Librarian.objects.get(library=library)
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
 
-# Example usage:
 if __name__ == "__main__":
-    # Create sample data
     author1 = Author.objects.create(name="J.K. Rowling")
     book1 = Book.objects.create(title="Harry Potter 1", author=author1)
     book2 = Book.objects.create(title="Harry Potter 2", author=author1)
@@ -36,7 +31,6 @@ if __name__ == "__main__":
     
     librarian1 = Librarian.objects.create(name="Ms. Smith", library=library1)
     
-    # Test the queries
     print("Books by J.K. Rowling:")
     for book in get_books_by_author("J.K. Rowling"):
         print(f"- {book.title}")
