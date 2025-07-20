a6b1bf5030ccc7417b7c2237c173fd701a6137fb
Evoless <amoogames519@gmail.com>
2025-07-20 19:44:02 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,8 +10,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all().select_related('author')
-        return context
--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
-app_name = 'relationship_app'
-
 urlpatterns = [
-    path('books/', list_books, name='book-list'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
