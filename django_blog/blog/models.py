a33f2b4c884857629ef82f7c06a8afd6c5a2ed55
Evoless <amoogames519@gmail.com>
2025-08-17 23:56:52 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,13 +6,11 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
