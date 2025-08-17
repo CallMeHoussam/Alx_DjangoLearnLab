99aa0b9dca6c93cbfc77340da8bca57fe577d1df
Evoless <amoogames519@gmail.com>
2025-08-17 23:20:02 +0100

    Implementing the Blog's User Authentication System

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -84,7 +84,8 @@ DATABASES = {
     }
 }
 
-
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'home'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
