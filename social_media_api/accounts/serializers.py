7e382b80dcb82a93f07e298e7acce39916929762
Evoless <amoogames519@gmail.com>
2025-08-23 10:59:17 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ('username', 'email', 'password', 'password_confirmation', 'bio', 'profile_picture')
+    
+    def validate(self, data):
+        if data['password'] != data['password_confirmation']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
     
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
+        validated_data.pop('password_confirmation')
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 class UserLoginSerializer(serializers.Serializer):
@@ -44,3 +49,4 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'bio', 'profile_picture', 'followers')
+        read_only_fields = ('username', 'email')
